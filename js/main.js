b32bb1954bd5cb7b0aa1ee55ef6d8d7e81f3729a
Abel <abeltareke@Abels-MacBook-Pro.local>
2020-03-16 21:16:06 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-let cardOne = "queen";
-let cardTwo = "King";
-let cardThree = "queen";
-let cardFour = "king";
 
-console.log("User flipped " + cardOne)
+let cards = ["queen", "queen", "king", "king"];
+
+let cardInPlay = [];
+
+let cardOne = "queen"
+let cardTwo = "king"
+
+cardInPlay.push(cardOne);
+cardInPlay.push(cardTwo);
+
+if(cardInPlay.length === 2){
+   alert("That's correct! There are two items.")
+} else {
+    alert("Sorry, Try again")
+}
+
+
+
+

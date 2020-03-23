bce1a4825f4e9690e74953c7dac67b352f38318b
Abel <abeltareke@Abels-MacBook-Pro.local>
2020-03-23 02:04:20 -0400

    Adding functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,61 @@
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+    {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
 
-let cardInPlay = [];
+
+let cardsInPlay = [];
 
 let cardOne = "queen"
 let cardTwo = "king"
 
-cardInPlay.push(cardOne);
-cardInPlay.push(cardTwo);
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+      } else {
+        alert("Sorry, try again.");
+      };
+}
 
-if(cardInPlay.length === 2){
-   alert("That's correct! There are two items.")
-} else {
-    alert("Sorry, Try again")
+function flipCard(cardId){
+    cardsInPlay.push(cardOne);
+    cardsInPlay.push(cardTwo);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    checkForMatch();
 }
 
 
 
+function createBoard(){
+    for(let i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    };
 
+};
+
+createBoard();
